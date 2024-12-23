0bb92f3a17be78226c6de98477923b0ff57a1a1c
yedidyar-ni <yedidya.r@next-insurance.com>
2024-12-23 22:01:38 +0200

    Enhance Toaster component in layout with custom toast options
    - Updated the Toaster component to include custom toast options for different message types (error, success, warning, info) with specific styling.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,17 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <WarRoomProvider>{children}</WarRoomProvider>
-        <Toaster position="bottom-center" />
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            classNames: {
+              error: "bg-red-100 text-red-600",
+              success: "bg-green-100 text-green-600",
+              warning: "bg-yellow-100 text-yellow-600",
+              info: "bg-blue-100 text-blue-600",
+            },
+          }}
+        />
       </body>
     </html>
   );
