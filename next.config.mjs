7255af71b5cdd847b846d510ac702a21fc81eb50
yedidyar-ni <yedidya.r@next-insurance.com>
2024-12-23 12:42:00 +0200

    Update Next.js configuration to enable static export and optimize image handling

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+};
 
 export default nextConfig;
