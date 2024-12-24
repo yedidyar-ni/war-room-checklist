0ebed50d93bbcd6119ad8b4b6908fb39e9a784a5
yedidyar-ni <yedidya.r@next-insurance.com>
2024-12-24 10:48:05 +0200

    Refactor Toaster component to use fixed dark theme and clean up unused code
    - Updated the Toaster component to set a fixed "dark" theme, removing reliance on dynamic theming.
    - Eliminated the unused theme variable and console log for a cleaner codebase.

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import { Toaster as Sonner } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme } = useTheme();
-
-  console.log(theme);
-
   return (
     <Sonner
       theme={"dark"}
